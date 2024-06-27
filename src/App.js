5cbdaf3e366fa1460aef0e42be79bd3b07131cc8
moudiki lionel <lmoudiki99@gmail.com>
2024-06-27 19:08:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Grid } from '@mui/material';
+ 
+import { SearchBar, VideoDetail } from './components';
+
+
+import youtube from './api/youtube';
+
+
+class App extends React.Component {
+    state = {
+        video: [],
+        selectedVideos: null
+    }
+    handleSubmit = async (searchTerm) =>{
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyD5n2mDxAokbtPoYcTClFBsRbtUdxHvlXU',
+                q: searchTerm, 
+            }
+            });
+
+       this.setState({ videos: response.data.items, selectedVideos: response.data.items[0]} });
+    }
+    render () {
+        return(
+            <Grid justify="center" container spacing={16}>
+               <Grid item xs={12}>
+                    <Grid container spacing={16} >
+                        <Grid item xs={12} >
+                            <SearchBar onFormSubmit = {this.handleSubmit}  />
+                        </Grid>
+                        <Grid item xs={8} >
+                            <VideoDetail />
+                        </Grid>
+                        <Grid item xs={4} >
+
+                        </Grid>
+                    </Grid>
+                </Grid> 
+            </Grid>
+
+               )
+    }
 }
 
 export default App;
